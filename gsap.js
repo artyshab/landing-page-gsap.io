63757c1ae2547cd4e8420f7dc4abaa7fd117f26f
artyshab <156674256+artyshab@users.noreply.github.com>
2024-03-16 14:49:02 +0100

    test2

--- gsap.js
+++ gsap.js
@@ -1,6 +1,6 @@
 
 
-    gsap.from('.first-section', { duration: 1, y: '-100%', ease: 'sine',  });
+    gsap.from('.first-section', { duration: 2, y: '-100%', ease: 'sine',  });
 
     gsap.from('.logo', { duration: 1, opacity: 0, delay: 1 });
     gsap.from('.links', { duration: 1, opacity: 0, delay: 1 });
