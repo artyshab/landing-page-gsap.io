ec12c7f93f746ac6495e22841974bb751464be88
artyshab <156674256+artyshab@users.noreply.github.com>
2024-03-16 14:34:26 +0100

    Initial commit

---
+++ gsap.js
@@ -0,0 +1,13 @@
+
+
+    gsap.from('.first-section', { duration: 1, y: '-100%', ease: 'sine',  });
+
+    gsap.from('.logo', { duration: 1, opacity: 0, delay: 1 });
+    gsap.from('.links', { duration: 1, opacity: 0, delay: 1 });
+    gsap.from('.text-image', { duration: 1, opacity: 0, delay: 2 });
+    gsap.from('.title', { duration: 1, opacity: 0, delay: 3 });
+    gsap.from('.cards', { duration: 1, opacity: 0, delay: 3 });
+    gsap.from('.third-section', { duration: 1, opacity: 0, delay: 3 });
+    gsap.from('.fourth-section', { duration: 1, opacity: 0, delay: 3 });
+    gsap.from('.footer', { duration: 1, opacity: 0, delay: 3 });
+
